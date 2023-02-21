b4a543fa1146f6494336a4c6754123f3f3805dd8
Mazaev Andrey <mazaev2000@gmail.com>
2023-02-22 01:28:08 +0300

    feat: header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+type Theme = "light" | "dark";
+const setColorTheme = (newTheme: Theme) => {
+  useColorMode().preference = newTheme;
+};
+</script>
+
+<template>
+  <header
+    class="transtition-all duration-400 fixed z-10 w-full bg-white/25 backdrop-blur-md dark:bg-[#20202380]"
+  >
+    <div class="mx-auto flex max-w-3xl items-center p-2">
+      <div class="mr-5 p-2">
+        <h1
+          class="font-['M_PLUS_Rounded_1c'] text-lg font-bold tracking-tighter"
+        >
+          Andrew Mazaev
+        </h1>
+      </div>
+      <ul class="flex flex-grow items-center gap-4">
+        <li class="p-2">Works</li>
+        <li class="p-2">Posts</li>
+        <li class="p-2">Uses</li>
+        <li class="p-2">Source</li>
+      </ul>
+      <button
+        class="ml-5"
+        @click="
+          setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')
+        "
+      >
+        <transition-group name="fade">
+          <svg
+            v-if="$colorMode.value == 'dark'"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8 text-gray-50"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+            />
+          </svg>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </transition-group>
+      </button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-40px);
+}
+.fade-leave-active {
+  position: absolute;
+}
+</style>
