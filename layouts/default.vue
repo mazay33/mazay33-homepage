e83f6a99275a4c3d585d159aa2aa8c54bdba6edd
Mazaev Andrey <mazaev2000@gmail.com>
2023-02-22 01:25:57 +0300

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col min-h-screen m-auto">
+    <TheHeader />
+    <div class="flex flex-1 mt-16">
+      <slot />
+    </div>
+    <!-- <TheFooter /> -->
+  </div>
+</template>
