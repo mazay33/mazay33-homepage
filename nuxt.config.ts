8003a48fee08cfc021792612918f50f215699e64
Mazaev Andrey <mazaev2000@gmail.com>
2023-02-21 23:31:08 +0300

    feat: light/dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: ''
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
