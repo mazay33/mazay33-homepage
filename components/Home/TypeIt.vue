96f8478363d3311dcbc2b08a93c9f3bac5da645e
Mazaev Andrey <mazaev2000@gmail.com>
2023-02-25 02:12:58 +0300

    feat: TypeIt component

---
+++ components/Home/TypeIt.vue
@@ -0,0 +1,31 @@
+<script>
+import TypeIt from "typeit";
+
+const element = ref("");
+if (element.value !== "") {
+  element.value === "";
+}
+export default {
+  mounted() {
+    new TypeIt("#element", {
+      speed: 60,
+      waitUntilVisible: true,
+      startDelay: 1500,
+      startDelete: true,
+      afterComplete: function (instance) {
+        instance.destroy();
+      },
+    })
+      .type("Hello! I'm a frontend developer based in Russia!")
+      .go();
+  },
+};
+</script>
+
+<template>
+  <div class="mx-auto mx-4 -mt-28 w-full max-w-[500px] px-2">
+    <div class="mb-6 rounded bg-white/10 p-3 text-center backdrop-blur-md">
+      <span class="text-xl" id="element" ref="element"></span>
+    </div>
+  </div>
+</template>
