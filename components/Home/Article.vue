5cc0f751398ac59a4c02c779a27e710fb3844013
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-02 12:47:25 +0300

    feat: article

--- components/Home/Article.vue
+++ components/Home/Article.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="flex items-start justify-between">
     <div class="flex flex-col">
@@ -8,9 +7,46 @@
       <p>Digital Craftsman ( Artist / Developer / Designer )</p>
     </div>
     <img
-      class="h-full max-h-[100px] w-full max-w-[100px] rounded-full border-[3px] border-gray-400 dark:border-gray-300 object-cover"
+      class="h-full max-h-[100px] w-full max-w-[100px] rounded-full border-[3px] border-gray-400 object-cover dark:border-gray-300"
       src="@/assets/img/image.jpg"
       alt=""
     />
+  </div>
+
+  <div class="flex flex-col justify-start">
+    <h3
+      class="my-4 text-xl font-bold underline decoration-gray-500 decoration-4 underline-offset-8"
+    >
+      Work
+    </h3>
+    <p class="text-justify indent-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit sint itaque
+      blanditiis eveniet quibusdam quia mollitia soluta inventore tempore earum
+      quisquam deleniti illum assumenda maxime, dignissimos necessitatibus nisi
+      porro odit veniam rerum placeat. Dolor atque ratione iste ullam incidunt,
+      sunt commodi? Odio quas et amet vitae eos fugiat ullam quia eligendi odit
+      doloremque a delectus omnis est minus, hic dolorem alias maxime.
+      Accusantium voluptatum, natus aliquid voluptate culpa facere laboriosam
+      enim. Voluptate eos autem in, est commodi fugit consequuntur ducimus
+      nesciunt aspernatur repellendus.
+    </p>
+    <div class="mt-4 flex justify-center">
+      <button
+        class="inline-flex items-center justify-center rounded bg-gray-300 py-2 px-4 font-bold text-gray-800 hover:bg-gray-400"
+      >
+        <span>My Portfolio</span>
+        <svg
+          viewBox="0 0 24 24"
+          focusable="false"
+          class="h-4 w-4"
+          aria-hidden="true"
+        >
+          <path
+            fill="currentColor"
+            d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"
+          ></path>
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
