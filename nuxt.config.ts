1aff3ac863f101a6676488b0ba86a6a443f12abb
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-05 16:53:19 +0300

    feat: page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts',  '@vueuse/nuxt'],
+  app: { pageTransition: { name: 'page', mode: 'out-in' } },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt'],
   colorMode: {
     classSuffix: ''
   },
