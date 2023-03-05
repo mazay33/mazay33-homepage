a454d954ad5aa285e57a5d643375c606ce519c5a
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-05 16:52:51 +0300

    feat: preload animation

--- app.vue
+++ app.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
+    <Loader v-if="isLoading" :isLoading="isLoading" />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 1000);
+});
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.7s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  transform: translateY(100vh);
+}
+</style>
---
+++ components/Loader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="loading" v-if="isLoading">
+    <div class="loading-spinner"></div>
+  </div>
+</template>
+
+<script>
+export default defineComponent({
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props) {
+    const isLoading = ref(props.isLoading);
+
+    watch(
+      () => props.isLoading,
+      (val) => {
+        isLoading.value = val;
+      }
+    );
+
+    return {
+      isLoading,
+    };
+  },
+});
+</script>
+
+<style>
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #202023;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.loading-spinner {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-top-color: #333;
+  animation: loading-spinner 1s linear infinite;
+}
+
+@keyframes loading-spinner {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
