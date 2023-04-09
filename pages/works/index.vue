1cd701dae543c6b95bb524c16727223e081f7c65
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-09 22:30:59 +0300

    feat: update works

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -24,19 +24,19 @@ const works = [
 <template>
   <div class="mx-auto w-full max-w-lg px-4">
     <h2 class="mb-4 font-['M_PLUS_Rounded_1c'] text-xl font-bold">Works</h2>
-    <div class="grid grid-cols-2 gap-6">
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
       <nuxt-link
         v-for="work in works"
         :key="work.title"
         :to="`/works/${work.title.toLowerCase().trim()}`"
-        class="flex flex-col items-center justify-center mb-auto"
+        class="mb-auto flex flex-col justify-center md:items-center"
       >
         <img
-          class="aspect-[16/9] max-h-32 max-w-full rounded object-cover object-top"
+          class="aspect-[16/9] max-w-full rounded-xl object-cover object-top shadow md:max-h-32"
           :src="work.img"
           alt=""
         />
-        <p class="mt-2 text-xl font-bold">{{ work.title }}</p>
+        <p class="mt-2 text-center text-xl font-bold">{{ work.title }}</p>
         <p class="text-center text-sm">
           {{ work.desc }}
         </p>
