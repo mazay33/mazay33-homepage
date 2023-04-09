02ac435806edd6a29cbe6d3430a8be20756ac5ba
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-10 00:00:51 +0300

    feat: new page

---
+++ pages/works/mazay33.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+useHead({
+  title: "Works - Mazay33",
+});
+</script>
+
+<template>
+  <div class="mx-auto w-full max-w-lg px-4">
+    <div class="mb-4 flex items-center gap-2">
+      <nuxt-link
+        to="/works"
+        class="text-pink-400 underline-offset-4 hover:underline"
+        >Works</nuxt-link
+      >
+      <span>></span>
+      <h2 class="font-['M_PLUS_Rounded_1c'] text-xl font-bold">Mazay33</h2>
+    </div>
+
+    <p class="text-justify indent-4">
+      
+    </p>
+    <ul class="m-4 flex flex-col gap-1">
+      <li>
+        <span
+          class="mr-2 rounded bg-emerald-300/30 px-1 py-[2px] text-xs font-bold uppercase text-emerald-300"
+          >Website</span
+        >
+        <a
+          target="_blank"
+          class="text-pink-400 underline-offset-4 hover:underline"
+          href="https://mazay33.com/"
+          >https://mazay33.com/</a
+        >
+      </li>
+      <li>
+        <span
+          class="mr-2 rounded bg-emerald-300/30 px-1 py-[2px] text-xs font-bold uppercase text-emerald-300"
+          >Stack</span
+        >
+        <span>Vue 3, SCSS</span>
+      </li>
+    </ul>
+    <img class="rounded-xl p-2" src="@/assets/images/mazay33-1.png" alt="" />
+    <img class="rounded-xl p-2" src="@/assets/images/mazay33-2.png" alt="" />
+    <img class="rounded-xl p-2" src="@/assets/images/mazay33-3.png" alt="" />
+    <img class="rounded-xl p-2" src="@/assets/images/mazay33-4.png" alt="" />
+  </div>
+</template>
