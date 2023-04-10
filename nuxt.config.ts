b2d604035bb91c807485756359f7bdf3a5287f90
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-10 20:26:30 +0300

    feat: install pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: { pageTransition: { name: 'page', mode: 'out-in' } },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@kevinmarrec/nuxt-pwa'],
+  pwa: {
+    workbox: {
+      enabled: true
+    }
+  },
   colorMode: {
     classSuffix: ''
   },
