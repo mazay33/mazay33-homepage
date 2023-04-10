1ead555b48b993f58593e5f7a6db46b016f9e57f
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-11 00:09:47 +0300

    feat: new page

---
+++ pages/skills.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto w-full max-w-lg px-4">
+    <h3
+      class="my-4 font-['M_PLUS_Rounded_1c'] text-xl font-bold underline decoration-gray-500 decoration-4 underline-offset-8"
+    >
+      My technology 
+    </h3>
+  </div>
+</template>
