6312cd9a62070e91f585ff12c6534a32aea1f2be
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-13 16:59:38 +0300

    feat: install nuxt-svgo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: { pageTransition: { name: 'page', mode: 'out-in' } },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@kevinmarrec/nuxt-pwa'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@kevinmarrec/nuxt-pwa', 'nuxt-svgo'],
   pwa: {
     workbox: {
       enabled: true
