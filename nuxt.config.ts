d2d129f08c14ff1d4daf0778586a67987c0c5d5f
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-14 18:32:10 +0300

    feat: some fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: { pageTransition: { name: 'page', mode: 'out-in' } },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@kevinmarrec/nuxt-pwa', 'nuxt-svgo'],
+  modules: ['@nuxtjs/color-mode', '@vueuse/nuxt', '@kevinmarrec/nuxt-pwa', 'nuxt-svgo'],
   pwa: {
     workbox: {
       enabled: true
-    }
-  },
-  svgo: {
-    svgoConfig: {
-      multipass: true,
-      plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              // customize default plugin options
-              inlineStyles: {
-                onlyMatchedOnce: false
-              },
-
-              // or disable plugins
-              removeDoctype: false,
-              removeViewBox: false
-            }
-          }
-        }
-      ]
     }
   },
   colorMode: {
@@ -40,10 +18,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  googleFonts: {
-    preload: true,
-    families: {
-      'M+PLUS+Rounded+1c': true,
-    }
-  }
 })
