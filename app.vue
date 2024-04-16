56f2c74b5e97155de44b53156ce42b5bcc9f5b40
mazay33 <mazaev2000@gmail.com>
2024-04-16 13:29:10 +0300

    feat: update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Loader v-if="isLoading" :isLoading="isLoading" />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -11,17 +10,16 @@
 useHead({
   title: "Andrew Mazaev - Homepage",
   meta: [
-    { name: "Andrew Mazaev - Homepage", content: "Frontend developer portfolio" },
-    { name: "description", content: "Andrew Mazaev's Frontend developer portfolio" },
+    {
+      name: "Andrew Mazaev - Homepage",
+      content: "Frontend developer portfolio",
+    },
+    {
+      name: "description",
+      content: "Andrew Mazaev's Frontend developer portfolio",
+    },
     { name: "yandex-verification", content: "e30623a7d948f5da" },
   ],
-});
-const isLoading = ref(true);
-
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 1000);
 });
 </script>
 
