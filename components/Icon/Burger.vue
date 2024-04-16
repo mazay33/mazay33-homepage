59b105bd18a558c997e0ad18ee4ceb9633b529af
mazay33 <mazaev2000@gmail.com>
2024-04-16 16:46:43 +0300

    feat: icons

---
+++ components/Icon/Burger.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    class="swap-off fill-current text-gray-800 dark:text-gray-50"
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="currentColor"
+      d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
